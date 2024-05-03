1cd1aa0f477c3a6d17d67bd22e9d2c13887d8836
rellagant <barbara.cs@gmail.com>
2024-05-03 00:52:03 -0400

    created and styled avatar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.scss';
+import { Avatar } from './components/Component/Avatar.jsx'
+import AvatarImage from './assets/images/Mohan-muruge.jpg'
+
 
 function App() {
   return (
-    <></>
+    <>
+    {/* <Avatar src={AvatarImage} alt="User Avatar" /> 
+    <Avatar/> */}
+    </>
   );
 }
 
