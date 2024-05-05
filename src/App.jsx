bc6f9b2d340be1de9ff748771bbc36fda8ee7df8
rellagant <barbara.cs@gmail.com>
2024-05-05 15:48:07 -0400

    pulled data from video details to populate video component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,24 @@ import { useState } from 'react';
 import videoDetails  from './data/video-details.json';
 import videos from './data/videos.json'
 import { Video } from './components/Component/Video.jsx'
+import { Article } from './components/Article/Article.jsx';
 
 // import { Avatar } from './components/Component/Avatar.jsx';
 // import AvatarImage from './assets/images/Mohan-muruge.jpg';
-  
+
+const convertTimeStamp = (timestamp) => {
+  console.log(timestamp);
+  const date = new Date(timestamp);
+  const dateFormat = {
+    // weekday: "short",
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+  };
+  const formattedDate = date.toLocaleDateString("en-US", dateFormat);
+  const time = formattedDate.replace(/,/g, "");
+  return time;
+};
 
 function App() {
 
@@ -26,6 +40,16 @@ function App() {
     // handleVideoSelect={handleVideoSelect}
     mainPoster={videoDetails[videoIndex].image}
     />
+    <Article
+    currentVideoIndex={[0]}
+    title={videoDetails[videoIndex].title}
+    channel={videoDetails[videoIndex].channel}
+    date={convertTimeStamp(videoDetails[videoIndex].timestamp)}
+    views={videoDetails[videoIndex].views}
+    likes={videoDetails[videoIndex].likes}
+    description={videoDetails[videoIndex].description}
+    />
+
     {/* "HI JOSEPH, do you have a technicolor dreamcoat?"
     <Avatar src={AvatarImage} alt="User Avatar" /> 
     <Avatar/> */}
@@ -38,4 +62,5 @@ export default App;
 
 //go back to restyle header search border - left margin on brainflix logo
 //go back to restyle mohan in avatar -- use the <> from under the header when you do
+//rename video to BEM in video component
 
