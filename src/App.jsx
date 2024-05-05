f568e8f2cc3816dd0fcfd27eb2a2c0554f1d9e4f
rellagant <barbara.cs@gmail.com>
2024-05-05 00:27:06 -0400

    created header component; styled header component; adjusted mixins to help style header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.scss';
-import './styles/partials/_globals.scss'
-import { Avatar } from './components/Component/Avatar.jsx'
-import AvatarImage from './assets/images/Mohan-muruge.jpg'
+import './styles/partials/_globals.scss';
+import { Header } from './components/Component/Header.jsx';
+// import { Avatar } from './components/Component/Avatar.jsx';
+// import AvatarImage from './assets/images/Mohan-muruge.jpg';
 
 
 function App() {
   return (
     <>
-    "HI JOSEPH, do you have a technicolor dreamcoat?"
+    <Header/>
+    {/* "HI JOSEPH, do you have a technicolor dreamcoat?"
     <Avatar src={AvatarImage} alt="User Avatar" /> 
-    <Avatar/>
+    <Avatar/> */}
     </>
   );
 }
---
+++ src/components/Component/Header.jsx
@@ -0,0 +1,43 @@
+import "./Header.scss";
+import LogoImg from "../../assets/images/icons/BrainFlix-logo.svg";
+import { Avatar } from "../../components/Component/Avatar";
+import UploadImg from "../../assets/images/icons/upload.svg";
+
+export function Header() {
+  return (
+    <>
+      <header className="header">
+        <div className="header__logo">
+          <a href="/">
+            <img src={LogoImg} alt="Brainflix Logo" />
+          </a>
+        </div>
+
+        <nav className="header__nav">
+          <input
+            className="header__search-input"
+            type="text"
+            placeholder="Search..."
+          />
+
+   
+
+          <button className="header__button" type="submit">
+            <img
+              className="header__button-icon"
+              src={UploadImg}
+              alt="Upload Icon"
+            />
+            <h3 className="header__button-text">UPLOAD</h3>
+          </button>
+
+          <Avatar/>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+//logo
+//searchbar - black search icon inside -- mohan beside that
+//upload button -- white + sign inside -- non functional
