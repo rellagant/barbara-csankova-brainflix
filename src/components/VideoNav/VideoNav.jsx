f8c5dbc794fca067efac0b9dcd85e5711f3a6304
rellagant <barbara.cs@gmail.com>
2024-05-05 23:06:33 -0400

    created and styled videonav component for mobile

--- src/components/VideoNav/VideoNav.jsx
+++ src/components/VideoNav/VideoNav.jsx
@@ -1,16 +1,23 @@
 import "./VideoNav.scss";
 
 export function VideoNav({ videos, id, handleVideoSelect }) {
-  console.log(videos);
+ 
   return (
-    <div>
+  
+    <div className="videos">
+        <h3 className="videos__header">Next Videos</h3>
       {videos.map((video, index) => {
         if (video.id !== id) {
           return (
-            <div key={index} onClick={() => handleVideoSelect(index)}>
-              <img src={video.image} alt="" />
-              <h2>{video.title}</h2>
-              <div>{video.channel}</div>
+            <div key={index} onClick={() => handleVideoSelect(index)} className="videos__container">
+              <div className="videos__left">
+              <img src={video.image} alt="Next Video" className="videos__video" />
+              </div>
+
+              <div className="videos__right">
+              <h2 className="videos__title">{video.title}</h2>
+              <div class="videos__comment">{video.channel}</div>
+              </div>
             </div>
           );
         }
