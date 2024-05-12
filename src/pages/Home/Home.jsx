39ebb9a1df8d9f5a4be9b8a4f45369ff192f5cb1
rellagant <barbara.cs@gmail.com>
2024-05-11 22:44:03 -0400

    made async calls within home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,38 +1,55 @@
 import "./Home.scss";
+import { convertTimeStamp } from "../../utils/convertTimestamp";
 // import '../../styles/partials/_globals';
-import videoDetails from "../../data/video-details.json";
-import videos from "../../data/videos.json";
 import { Video } from "../../components/Video/Video";
 import { Article } from "../../components/Article/Article";
 import { Comments } from "../../components/Comments/Comments";
 import { VideoNav } from "../../components/VideoNav/VideoNav";
-import { useState } from "react";
-
-
-export const convertTimeStamp = (timestamp) => {
-    console.log(timestamp);
-    const date = new Date(timestamp);
-    const dateFormat = {
-      month: "2-digit",
-      day: "2-digit",
-      year: "numeric",
-    };
-    const formattedDate = date.toLocaleDateString("en-US", dateFormat);
-    const time = formattedDate.replace(/,/g, "");
-    return time;
-  };
+import { useEffect, useState } from "react";
+import { BrainFlixApi, apiKey } from "../../classes/brainflixapi";
+import { useParams } from "react-router-dom";
 
 export function Home() {
-    const [videoIndex, setVideoIndex] = useState(0);
-    const handleVideoSelect = (transformIndex) => {
-      setVideoIndex(transformIndex);
-    };
-  
 
-    return (
-        <>
-         <Video mainPoster={videoDetails[videoIndex].image} />
-      <Article
+  const [videos, setVideos] = useState([]);
+    const [videoDetails, setVideoDetails] = useState({});
+    // const [hasFetchError, setHasFetchError] = useState(false);
+    const { id } = useParams();
+
+
+    useEffect(() => {
+
+      const fetchVideos = async () => {
+        const getBrainFlixApi = new BrainFlixApi(apiKey);
+
+        try {
+          const response = await getBrainFlixApi.getVideos(); //list of videos (no details)
+          setVideos(response.data);
+
+
+          const ichibanVideoId = response.data[0].id
+          const hotdog = await getBrainFlixApi.getVideoDetails(id || ichibanVideoId); //details of one single video
+
+          setVideoDetails(hotdog.data);
+
+        } catch(error) {
+          console.error("not for you", error);
+          // setHasFetchError(true);
+        }
+      }
+      fetchVideos();
+    }, [id])
+  // const [videoIndex, setVideoIndex] = useState(0);
+  // const handleVideoSelect = (transformIndex) => {
+  //   setVideoIndex(transformIndex);
+  // };
+
+  return (
+  
+    <>
+      <Video mainPoster={videoDetails.image} />
+
+      {/* <Article
         title={videoDetails[videoIndex].title}
         channel={videoDetails[videoIndex].channel}
         date={convertTimeStamp(videoDetails[videoIndex].timestamp)}
@@ -41,17 +58,16 @@ export function Home() {
         description={videoDetails[videoIndex].description}
       />
 
-      <Comments comments={videoDetails[videoIndex].comments} />
+      <Comments comments={videoDetails[videoIndex].comments} /> */}
 
       <VideoNav
         videos={videos}
-        key={videoDetails[videoIndex].id}
-        id={videoDetails[videoIndex].id}
-        handleVideoSelect={handleVideoSelect}
+        // key={videoDetails[videoIndex].id}
+        // id={videoDetails[videoIndex].id}
+        // handleVideoSelect={handleVideoSelect}
       />
 
-     
-        {/* Everything except for header comes here from app.jsx and is replaced in app.jsx with the home route, plus the video player page stuff */}
-        </>
-    )
+      {/* Everything except for header comes here from app.jsx and is replaced in app.jsx with the home route, plus the video player page stuff */}
+    </>
+  );
 }
