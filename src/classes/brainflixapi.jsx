9743e885909c9b93c62c90158a55e3f6633536f7
Barbara Csankova <barbara.cs@gmail.com>
2024-05-19 13:58:57 -0400

    refactored api class constructor to reference new brainflix api server address

--- src/classes/brainflixapi.jsx
+++ src/classes/brainflixapi.jsx
@@ -1,19 +1,22 @@
 import axios from "axios";
 
-export const apiKey = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
+console.log(process.env);
 
 export class BrainFlixApi {
-    constructor(apiKey) {
-        this.apiKey = apiKey
-        this.baseUrl = "https://unit-3-project-api-0a5620414506.herokuapp.com"
+    constructor() {
+        this.baseUrl = API_URL
+        console.log(this.baseUrl);
     }
 
     async getVideos() {
-        return await axios.get(`${this.baseUrl}/videos/?api_key=${this.apiKey}`);
+        return await axios.get(`${this.baseUrl}/videos/`);
+        
     }
 
     async getVideoDetails(id) {
-        return await axios.get(`${this.baseUrl}/videos/${id}?api_key=${this.apiKey}`);
+        return await axios.get(`${this.baseUrl}/videos/${id}`);
+
     }
 
 }
