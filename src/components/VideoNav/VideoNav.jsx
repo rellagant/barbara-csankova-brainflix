d48622d8e9da918bb8fd146ef96ac5f0dc7b4a2d
Barbara Csankova <barbara.cs@gmail.com>
2024-05-20 22:28:15 -0400

    added handleclick function to scroll to top of page when a video in nav is clicked

--- src/components/VideoNav/VideoNav.jsx
+++ src/components/VideoNav/VideoNav.jsx
@@ -2,25 +2,34 @@ import "./VideoNav.scss";
 import { Link } from "react-router-dom";
 
 export function VideoNav({ videos, id }) {
- 
+  const handleVideoClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-  
     <div className="videos">
-        <h3 className="videos__header">Next Videos</h3>
+      <h3 className="videos__header">Next Videos</h3>
       {videos.map((video, index) => {
         if (video.id !== id) {
           return (
-            <Link to={`/${video.id}`} key={index} >
-            <div className="videos__container">
-              <div className="videos__left">
-              <img src={video.image} alt="Next Video" className="videos__video" />
-              </div>
+            <Link to={`/${video.id}`} key={index} onClick={handleVideoClick}>
+              <div className="videos__container">
+                <div className="videos__left">
+                  <img
+                    src={video.image}
+                    alt="Next Video"
+                    className="videos__video"
+                  />
+                </div>
 
-              <div className="videos__right">
-              <h2 className="videos__title">{video.title}</h2>
-              <div className="videos__comment">{video.channel}</div>
+                <div className="videos__right">
+                  <h2 className="videos__title">{video.title}</h2>
+                  <div className="videos__comment">{video.channel}</div>
+                </div>
               </div>
-            </div>
             </Link>
           );
         }
@@ -29,5 +38,3 @@ export function VideoNav({ videos, id }) {
     </div>
   );
 }
-
-// don't need a return
